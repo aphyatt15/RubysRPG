4fff488b5cfa2371e81e554d89611caf77dabb95
Adam Hyatt <adamhyatt@AdamsMacBookPro.local>
2019-08-09 16:57:25 -0700

    first commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 1.0f;
+
+    float timer;
+    float direction = 1;
+    bool broken = true;
+
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!broken) return;
+
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+        
+        Vector2 position = rigidbody2d.position;
+        if (vertical)
+        {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            position.y = position.y + speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            position.x = position.x + speed * direction * Time.deltaTime;
+        }
+        
+        rigidbody2d.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+    public void Fix()
+    {
+        animator.SetTrigger("Fixed");
+        broken = false;
+        rigidbody2d.simulated = false;
+    }
+
+}
